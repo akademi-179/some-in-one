49f3d574d5ce16d87d42700ec1555068ecb418d6
Orcun Selbasan <lutfuorcunselbasan@gmail.com>
2022-06-01 21:55:06 +0300

    QR klasörleri düzenlendi

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:some_in_one/screens/five.dart';
 import 'package:some_in_one/screens/four.dart';
 import 'package:some_in_one/screens/main_screen.dart';
+import 'package:some_in_one/screens/qr_scanner/home_screen.dart';
 import 'package:some_in_one/screens/three.dart';
 import 'package:some_in_one/screens/two.dart';
 
@@ -11,6 +12,6 @@ class Routes {
     '/two': (context) => const Two(),
     '/three': (context) => const Three(),
     '/four': (context) => const Four(),
-    '/five': (context) => const Five(),
+    '/five': (context) => HomeScreen(),
   };
 }
